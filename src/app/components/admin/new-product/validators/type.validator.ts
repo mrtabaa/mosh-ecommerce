bae548f2f6241f51dda5accac97f2aa52007a8fc
Reza Taba <admin@rezataba.com>
2021-01-02 20:48:55 +0300

    cleared code

--- src/app/components/admin/new-product/validators/type.validator.ts
+++ src/app/components/admin/new-product/validators/type.validator.ts
@@ -5,7 +5,6 @@ export class TypeValidator {
     // check Type uniqueness
     static checkUniqueType(types: string[]): ValidationErrors | null {
         return (control: AbstractControl) => {
-            console.log(types);
             if (types && types.includes((control.value as string).toLowerCase())) {
                 return { typeNotUnique: true };
             }

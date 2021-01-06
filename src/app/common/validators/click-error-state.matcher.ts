b34c6bace1f256b50d5ec97c6f2d459a643c5caa
Reza Taba <admin@rezataba.com>
2021-01-07 02:00:55 +0300

    generalized clickErrorStateMatcher

--- src/app/common/validators/click-error-state.matcher.ts
+++ src/app/common/validators/click-error-state.matcher.ts
@@ -3,6 +3,6 @@ import { ErrorStateMatcher } from '@angular/material/core';
 
 export class ClickErrorStateMatcher implements ErrorStateMatcher {
     isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
-        return !!(control.touched && form.hasError('noTypeSelected'));
+        return !!(control.touched && form.hasError('noPrerequisiteSelected'));
     }
 }
